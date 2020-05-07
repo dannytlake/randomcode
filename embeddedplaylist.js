4907b508245666400cb75e1ba2a95dba466c7d68
danny lake <dannytlake@gmail.com>
2020-05-07 14:11:38 -0700

    formatting

--- embeddedplaylist.js
+++ embeddedplaylist.js
@@ -11,13 +11,13 @@ for (n = 0; n < v.length; n++) {
     : 'http://i.ytimg.com/vi/' + v[n].id + '/hqdefault.jpg';
 }
 
-if (v.length > 0) {
-  s = document.createElement('style');
-  s.type = 'text/css';
-  s.innerHTML =
-    '.youtube_embedded_playlist {background-color:#000;max-width:100%;overflow:hidden;position:relative;cursor:hand;cursor:pointer}.youtube_embedded_playlist .thumb{bottom:0;display:block;left:0;margin:auto;max-width:100%;position:absolute;right:0;top:0;width:100%;height:auto}.youtube_embedded_playlist .play{filter:alpha(opacity=80);opacity:.8;height:77px;left:50%;margin-left:-38px;margin-top:-38px;position:absolute;top:50%;width:77px;background:url("https://lh3.ggpht.com/vo4W82YNfpJDsttqn-22YsLtEJjmOtIB-54yIxR5wQA0Ucs5leNIu-W8iEmyY8-Pf7RWHk4=w64") no-repeat}';
-  document.body.appendChild(s);
-}
+// if (v.length > 0) {
+//   s = document.createElement('style');
+//   s.type = 'text/css';
+//   s.innerHTML =
+//     '.youtube_embedded_playlist {background-color:#000;max-width:100%;overflow:hidden;position:relative;cursor:hand;cursor:pointer}.youtube_embedded_playlist .thumb{bottom:0;display:block;left:0;margin:auto;max-width:100%;position:absolute;right:0;top:0;width:100%;height:auto}.youtube_embedded_playlist .play{filter:alpha(opacity=80);opacity:.8;height:77px;left:50%;margin-left:-38px;margin-top:-38px;position:absolute;top:50%;width:77px;background:url("https://lh3.ggpht.com/vo4W82YNfpJDsttqn-22YsLtEJjmOtIB-54yIxR5wQA0Ucs5leNIu-W8iEmyY8-Pf7RWHk4=w64") no-repeat}';
+//   document.body.appendChild(s);
+// }
 
 for (n = 0; n < v.length; n++) {
   y = v[n];
